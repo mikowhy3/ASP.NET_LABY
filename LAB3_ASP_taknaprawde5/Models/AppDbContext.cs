0c2cbf28a19a3a31dd19b48160698dd4af981a1b
zad_data_uro <mikolaj.babicz@microsoft.wsei.edu.pl>
2024-11-12 21:23:26 +0100

    wypchanie_lab5

---
+++ LAB3_ASP_taknaprawde5/Models/AppDbContext.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace LAB3_SIWON.Models
+{
+    public class AppDbContext:DbContext
+    {
+
+        public DbSet<ContactEntity>Contacts { get; set; }
+
+        private string DbPath { get; set; }
+            
+
+        public AppDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "contacts.db");
+        }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString: $"Data source={DbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContactEntity>()
+                .HasData(
+                new ContactEntity
+                {
+                    Id=1,
+                    First_Name="Adam",
+                    Last_Name="Kowal",
+                    Email="adam@wsei.edu.pl",
+                    PhoneNumber="123456789",
+                    BirthDate= new DateOnly(year:2000,month:10, day:23)
+                },
+                 new ContactEntity
+                 {
+                     Id = 2,
+                     First_Name = "Ada",
+                     Last_Name = "Wal",
+                     Email = "ada@wsei.edu.pl",
+                     PhoneNumber = "123457789",
+                     BirthDate = new DateOnly(year: 2001, month: 10, day: 23)
+                 }
+
+
+                );
+        }
+    }
+}
