8e516ee4e63d6c5931fa88617caf103d3f7105fc
zad_data_uro <mikolaj.babicz@microsoft.wsei.edu.pl>
2024-11-13 20:06:34 +0100

    Zmiana nazwy folderu na 'Aktualizowane_zaj'

--- LAB3_ASP_taknaprawde5/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LAB3_SIWON.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
